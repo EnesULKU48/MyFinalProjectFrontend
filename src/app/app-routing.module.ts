f496101846fa8f1b27e6a81767963665ce112682
Enes ÜLKÜ <enesulku4834@gmail.com>
2021-04-04 22:07:14 +0300

    18. gün sonu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [//pathmach yazmasak da olur versiyonla ilgili 
+ {path:"", pathMatch:"full", component:ProductComponent}, //  4200 gelirse de bu componenti ver
+ {path:"products", component:ProductComponent}, // 4200/product gelirse bu componenti ver
+ {path:"products/category/:categoryId", component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/responseModel.ts
@@ -0,0 +1,5 @@
+//burası base miz
+export interface ResponseModel{
+    success:boolean,
+    message:string
+}
